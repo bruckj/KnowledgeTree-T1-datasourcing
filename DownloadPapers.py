0c84a3868d5be0ac7436d7022ff6cd5e9dd3fd43
dscharnitzky <37212791+dscharnitzky@users.noreply.github.com>
2018-04-02 11:30:30 +0200

    Update DownloadPapers.py

--- DownloadPapers.py
+++ DownloadPapers.py
@@ -7,59 +7,102 @@ def PDFGetter(PaperListTxT, WhereToSave):
     """
     import http.client
     import json
+    import os
+    import datetime
+    #Cikkek szama
+    nPapers = 0
+    #Letoltott pdf-ek szama
+    nPDFs = 0
+    #HTTP 404 hibat adott pdf linkek szama
+    notFounds = 0
+    #Egyeb hibak szama
+    otherFails = 0
     with open(PaperListTxT,"r") as txt:
-        #Cikkek szama
-        nPapers = 0
-        #Letoltott pdf-ek szama
-        nPDFs = 0
-        #HTTP 404 hibat adott pdf linkek szama
-        notFounds = 0
-        #Egyeb hibak szama
-        otherFails = 0
         #Kapcsolatok nyilvantartasara hasznalt szotar
         conn = {}
+        #Mar letezo fajlok listaja
+        files = os.listdir(WhereToSave)
         #A fajlban soronkent egy cikk szerepel
         for line in txt:
             paper = json.loads(line)
             if not paper is None:
                 nPapers += 1
-                PDF = paper.get("pdf")
-                if not PDF is None:
-                    error = 0
-                    while True:
-                        #A linkek //valami.org/cikk.pdf alakuak
-                        host = PDF.split("/")[2]
-                        try:
-                            if not host in conn:
-                                #A kapcsolat a fuggveny futasa alatt fenall
-                                conn[host] = http.client.HTTPConnection(host)
-                            #A pdf letoltese HTTP GET uzenettel
-                            conn[host].request("GET",PDF[2+len(host):])
-                            res = conn[host].getresponse()
-                            if(res.status != 404):
-                                data = res.read()
-                                #static.aminer.org helyrol letoltott pdf-eknel elofordul, hogy egy -napi limit betelt- uzenetet ad vissza a pdf
-                                #helyett, ezek az uzenetek 206 meretuek es mndig ugyanannal a linknel fordulnak elo
-                                if(len(data)>0 and len(data) != 206):
-                                    f = open(WhereToSave+"\\"+paper.get("id")+".pdf","wb")
-                                    f.write(data)
-                                    f.close()
-                                    nPDFs += 1
-                                    break
+                if paper.get("id")+".pdf" not in files:
+                    PDF = paper.get("pdf")
+                    if not PDF is None:
+                        error = 0
+                        while True:
+                            #A linkek //valami.org/cikk.pdf alakuak
+                            host = PDF.split("/")[2]
+                            try:
+                                if not host in conn:
+                                    #A kapcsolat a fuggveny futasa alatt fenall
+                                    conn[host] = http.client.HTTPConnection(host)
+                                #A pdf letoltese HTTP GET uzenettel
+                                conn[host].request("GET",PDF[2+len(host):])
+                                res = conn[host].getresponse()
+                                if(res.status != 404):
+                                    data = res.read()
+                                    #static.aminer.org helyrol letoltott pdf-eknel elofordul, hogy egy -napi limit betelt- uzenetet ad vissza a pdf
+                                    #helyett, ezek az uzenetek 206 meretuek es mndig ugyanannal a linknel fordulnak elo
+                                    if(len(data)>0 and len(data) != 206):
+                                        f = open(WhereToSave+"\\"+paper.get("id")+".pdf","wb")
+                                        f.write(data)
+                                        f.close()
+                                        nPDFs += 1
+                                        break
+                                    else:
+                                        raise http.client.ResponseNotReady
                                 else:
-                                    raise http.client.ResponseNotReady
-                            else:
-                                notFounds += 1
-                                break
-                        except http.client.HTTPException:
-                            #Hibakezeles: kapcsolat ujra felepitese. Nem probalkozik a vegtelensegig, erre szolgal az error valtozo
-                            if(error > 5):
-                                otherFails += 1
-                                break
-                            conn[host].close()
-                            conn[host] = http.client.HTTPConnection(host)
-                            error += 1
+                                    notFounds += 1
+                                    break
+                            except (http.client.HTTPException,ConnectionResetError):
+                                #Hibakezeles: kapcsolat ujra felepitese. Nem probalkozik a vegtelensegig, erre szolgal az error valtozo
+                                if(error > 5):
+                                    otherFails += 1
+                                    break
+                                conn[host].close()
+                                conn[host] = http.client.HTTPConnection(host)
+                                error += 1
+                #Mar levan toltve a fajl
+                else:
+                    nPDFs += 1
         #Nehany nagyon egyszeru statisztika kiirasa, nyugodtan kikommentelheto
-        print("Papers: "+str(nPapers)+"; PDFs: "+str(nPDFs)+"; not found: "+str(notFounds)+"; unknown errors: "+str(otherFails))
-#Pelda kod a fuggveny futtatasara
-#PDFGetter("c:\\Szakdoga\\aminer_papers_0.txt","c:\\Szakdoga")
+        print(str(datetime.datetime.now())+": Papers: "+str(nPapers)+"; PDFs: "+str(nPDFs)+"; not found: "+str(notFounds)+"; unknown errors: "+str(otherFails))
+    return nPapers, nPDFs, notFounds, otherFails
+def DownloadPapers(textDir,PDFDir):
+    """
+    textDir: A text fajlok helye.
+    PDFDir: A mappa, ahova a PDF-ek keruljenek (ezen belul text fajlokent lesz egy almappa).
+    Beolvassa a text fajlokat sorban es meghivja veluk a PDFGetter()-t
+    """
+    import os
+    import time
+    start = time.time()
+    os.makedirs(name=PDFDir, exist_ok=True)
+    files = os.listdir(textDir)
+    #Cikkek szama
+    nPapers = 0
+    #Letoltott pdf-ek szama
+    nPDFs = 0
+    #HTTP 404 hibat adott pdf linkek szama
+    notFounds = 0
+    #Egyeb hibak szama
+    otherFails = 0
+    for elem in files:
+        #csak a .txt fajlok kellenek
+        if(elem.rsplit(".")[-1] == "txt"):
+            dirname = elem[0:len(elem)-4]
+            #mappa letrehozasa a text fajl nevevel
+            os.makedirs(name=PDFDir+"\\"+dirname, exist_ok=True)
+            #PDF-ek letoltese
+            nPapersT, nPDFsT, notFoundsT, otherFailsT = PDFGetter(textDir+"\\"+elem,PDFDir+"\\"+dirname)
+            nPapers += nPapersT
+            nPDFs += nPDFsT
+            notFounds += notFoundsT
+            otherFails += otherFailsT
+    #Globalis statisztika kiirasa (kikommentelheto)
+    print("Done! Statistics:\nPapers: "+str(nPapers)+"; PDFs: "+str(nPDFs)+"; not found: "+str(notFounds)+"; unknown errors: "+str(otherFails))
+    print("Time [s]: "+str(time.time()-start))
+#A fuggveny meghivasa:
+DownloadPapers("c:\\Szakdoga\\Texts","c:\\Szakdoga\\asdPDFs2")
