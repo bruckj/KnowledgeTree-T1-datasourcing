b6a5786716e76f774af1772300113d392e10551c
dscharnitzky <37212791+dscharnitzky@users.noreply.github.com>
2018-04-27 13:54:02 +0200

    PDF-ek átrendezése

---
+++ Move.py
@@ -0,0 +1,35 @@
+#Athelyezi a pdf-eket az almappakbol, majd torli az ures mappakat
+import os
+startdir="/mnt/datasharepoint-pdf/openacademic"
+dirs = os.listdir(startdir)
+moveCount = 0
+idCollision = 0
+#Fajlok athelyezese:
+for dir in dirs:
+    if(os.path.isdir(startdir+"/"+dir)):
+        pdfs = os.listdir(startdir+"/"+dir)
+        for pdf in pdfs:
+            try:
+                if(os.path.isdir(startdir+"/"+dir+"/"+pdf)):
+                   continue
+                #Az athelyezes itt tortenik
+                os.rename(startdir+"/"+dir+"/"+pdf,startdir+"/"+pdf)
+                moveCount += 1
+            except NotADirectoryError:
+                continue
+            except FileExistsError:
+                #Ha mar letezik a fajl, akkor ket kulonbozo fajlnak egy id-je van (ugyanaz a neve)
+                with open("./MoveLog.txt","a") as log:
+                    log.write(dir+"/"+pdf+"\n")
+                idCollision += 1
+#Ures mappak torlese:
+for dir in dirs:
+    if(os.path.isdir(startdir+"/"+dir)):
+        if(len(os.listdir(startdir+"/"+dir)) == 0):
+            try:
+                os.removedirs(startdir+"/"+dir)
+            except FileNotFoundError:
+                continue
+print("Moved: "+str(moveCount)+" id collision: "+str(idCollision)+".")
+if(idCollision > 0):
+    print("See \"./MoveLog.txt\" for details.")
